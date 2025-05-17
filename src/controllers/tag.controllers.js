b2dd6f594bf993a574fd57d37c06fc4437798b34
robertoCGaleano <robertocarlos.galeano@estudiantes.unahur.edu.ar>
2025-05-16 22:48:05 -0300

    actualizando codigo de la rama

--- src/controllers/tag.controllers.js
+++ src/controllers/tag.controllers.js
@@ -1,15 +1,25 @@
 const { Tag } = require('../db/models')
-
+// Get
 const getTags = async (_, res) => {
   const data = await Tag.findAll({})
   res.status(200).json({ data })
 }
-
+// Get
 const getTagById = async (req, res) => {
   const data = await Tag.findByPk(req.params.id)
   res.status(200).json(data)
 }
-
+// Get para ver los posts que tienen el tag del id x
+const verPosts = async (req, res) => {
+  const { id } = req.params
+  const tag = await Tag.findByPk(id)
+  if (!tag) {
+    return res.status(404).json({ error: 'Tag no encontrado' })
+  }
+  const posts = await tag.getPosts({ joinTableAttributes: [] })
+  res.status(200).json(posts)
+}
+// Post
 const createTag = async (req, res) => {
   try {
     const newTag = await Tag.create(req.body)
@@ -18,21 +28,21 @@ const createTag = async (req, res) => {
     res.status(400).json({ error: e })
   }
 }
-
+// Put
 const updateTagById = async (req, res) => {
   const id = req.params.id
   const newDescription = req.body.description
   const tagToUpdate = await Tag.findByPk(id)
   tagToUpdate.description = newDescription
   await tagToUpdate.save()
-  res.status(200).json(tagToUpdate)
+  res.status(201).json(tagToUpdate)
 }
-
+// Delete
 const deleteTagById = async (req, res) => {
   const data = await Tag.findByPk(req.params.id)
   const removed = await data.destroy()
-  res.status(200).json(removed)
+  res.status(201).json(`Etiqueta eliminada exitosamente ${removed}`)
 }
 
 // Exportacion de todas las funciones
-module.exports = { getTags, createTag, getTagById, deleteTagById, updateTagById }
+module.exports = { getTags, createTag, getTagById, deleteTagById, updateTagById, verPosts }
--- src/middlewares/tag.middlewares.js
+++ src/middlewares/tag.middlewares.js
@@ -1,1 +1,16 @@
 const { Tag } = require('../db/models')
+
+// Verifica si el tag recibido ya existe
+const existTag = async (req, res, next) => {
+  try {
+    const newTag = req.body.description
+    const descriptionExist = await Tag.findOne({ where: { description: newTag } })
+    if (descriptionExist) return res.status(400).json({ message: 'Ya existe el tag' })
+    next()
+  } catch (e) {
+    console.log(`Error: ${e}`)
+    res.status(500).json({ error: e })
+  }
+}
+
+module.exports = { existTag }
