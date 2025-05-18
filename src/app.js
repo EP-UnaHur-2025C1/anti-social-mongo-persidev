f255a7cefad894df8ec2f6010fd66f14b816a29a
brendalera <brendalera17@gmail.com>
2025-05-18 20:38:57 -0300

    corrigiendo

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use("/api-docs/", swaggerUI.serve, swaggerUI.setup(openapiSpecification));
 
 // Listo mi aplicacion al puerto
 app.listen(PORT, async () => {
-  await db.sequelize.sync({ force: true });
+  //await db.sequelize.sync({ force: true });
   console.log(`Aplicacion corriendo en el puerto ${PORT}`);
 });
 // Exporto mi aplicacion
