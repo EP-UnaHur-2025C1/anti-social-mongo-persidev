726013fa5d266937da5a551f55dd2b89163a5a9e
MeliAlvarez14 <melialvarez180@gmail.com>
2025-05-18 00:39:22 -0300

    corrigiendo documentacion de Comment

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,35 +1,51 @@
 const { Comment } = require('../db/models')
 
 const getAllComments = async (_, res) => {
-  const data = await Comment.findAll({})
-  res.status(200).json(data)
+  try {
+    const data = await Comment.findAll({})
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor al intentar obtener los comentarios', error })
+  }
 }
 
 const getCommentById = async (req, res) => {
-  const data = await Comment.findByPk(req.params.id)
-  res.status(200).json(data)
+  try {
+    const data = await Comment.findByPk(req.params.id)
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor al obtener un comentario por ID', error })
+  }
 }
 
 const createComment = async (req, res) => {
   try {
     const newComment = await Comment.create(req.body)
     res.status(201).json(newComment)
-  } catch (e) {
-    res.status(400).json({ error: e })
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor al crear un comentario', error })
   }
 }
 
 const updateComment = async (req, res) => {
-  const comment = await Comment.findByPk(req.params.id)
-  comment.content = req.body.content
-  await comment.save()
-  res.status(200).json(comment)
+  try {
+    const comment = await Comment.findByPk(req.params.id)
+    comment.content = req.body.content
+    await comment.save()
+    res.status(200).json(comment)
+  } catch (error) {
+    res.status(404).json({ message: 'Comentario no encontrado', error })
+  }
 }
 
 const deleteCommentById = async (req, res) => {
-  const comment = await Comment.findByPk(req.params.id)
-  const removed = await comment.destroy()
-  res.status(200).json(removed)
+  try {
+    const comment = await Comment.findByPk(req.params.id)
+    const removed = await comment.destroy()
+    res.status(200).json(removed)
+  } catch (error) {
+    res.status(404).json({ message: 'Comentario no encontrado', error })
+  }
 }
 
 module.exports = { getAllComments, getCommentById, createComment, updateComment, deleteCommentById }
