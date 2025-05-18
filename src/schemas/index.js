7b6bbb9fc113d8f5046812b7f2a8fdfb302dadb8
brendalera <brendalera17@gmail.com>
2025-05-18 01:10:38 -0300

    agregando cambios de post-tag

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,6 +1,13 @@
-const userSchema = require('./user.schema')
-const postSchema = require('./post.schema')
-const tagSchema = require('./tag.schema')
-const commentSchema = require('./comment.schema')
+const userSchema = require("./user.schema");
+const postSchema = require("./post.schema");
+const tagSchema = require("./tag.schema");
+const commentSchema = require("./comment.schema");
+const postTagSchema = require("./tagpost.schema");
 // Exportacion
-module.exports = { userSchema, postSchema, tagSchema, commentSchema }
+module.exports = {
+  userSchema,
+  postSchema,
+  tagSchema,
+  commentSchema,
+  postTagSchema,
+};
