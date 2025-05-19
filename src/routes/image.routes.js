0f5dd347603b1b8833688868dc98f65c164ac0ec
MeliAlvarez14 <melialvarez180@gmail.com>
2025-05-19 02:02:10 -0300

    Arreglos en la documentacion de Image

--- src/routes/image.routes.js
+++ src/routes/image.routes.js
@@ -16,12 +16,6 @@ imageRoutes.post('/',
   genericMiddlewares.validatorSchema(imageSchema),
   imageControllers.createImage)
 
-// imageRoutes.put('/:id',
-//   genericMiddlewares.validateID(),
-//   genericMiddlewares.existID(Image),
-//   genericMiddlewares.validatorSchema(imageSchema),
-//   imageControllers.updateImage)
-
 imageRoutes.delete('/:id',
   genericMiddlewares.validateID(),
   genericMiddlewares.existID(Image),
