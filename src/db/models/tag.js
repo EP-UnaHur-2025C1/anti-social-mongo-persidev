dcc444b5467b84f1396435b37e7aef4e41ed56cd
robertoCGaleano <robertocarlos.galeano@estudiantes.unahur.edu.ar>
2025-06-15 11:17:46 -0300

    agregando modelo tag

--- src/db/models/tag.js
+++ src/db/models/tag.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
-const tagSchema = new mongoose.Schema()
+const tagSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    unique: [true, 'La descripcion debe ser unica'],
+    required: [true, 'La descripcion es requerida']
+  }
+}, { strict: false }) // para que sea flexible a crear con mas atributos
 
-// Exportacion
+// creo el modelo y lo exporto
 module.exports = mongoose.model('Tag', tagSchema)
