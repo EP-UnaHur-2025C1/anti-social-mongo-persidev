91c6cd6016fcd457f598940ef22b0bba5c3af755
MeliAlvarez14 <melialvarez180@gmail.com>
2025-06-15 22:35:46 -0300

    agregando schema de Comment

--- src/db/models/comment.js
+++ src/db/models/comment.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose')
 
-const commentSchema = new mongoose.Schema()
+const commentSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        publicationDate: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        collection: 'posteos',
+    }
+  )
+    
+    commentSchema.virtual('isVisible').get(function () {
+        const xMonths = process.env.MONTHS || 6
+        const yearDifference = new Date().getFullYear() - new Date(this.get('publicationDate')).getFullYear()
+        const monthDifference = new Date().getMonth() - new Date(this.get('publicationDate')).getMonth()
+        return (yearDifference * 12 + monthDifference) < xMonths 
+    })
 
-// Exportacion
 module.exports = mongoose.model('Comment', commentSchema)
