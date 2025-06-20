3650cac06c3067af208854941a2e2b81bd02a144
brendalera <brendalera17@gmail.com>
2025-06-19 22:17:25 -0300

    agregando colecciones de post y sacando todo lo de postTag

--- src/db/models/posttag.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const postTagSchema = new mongoose.Schema()
-
-// Exportacion
-module.exports = mongoose.model('PostTag', postTagSchema)
--- src/schemas/tagpost.schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi')
-
-const postTagSchema = Joi.object({
-  PostId: Joi.number().integer().required(),
-  TagId: Joi.number().integer().required()
-})
-
-module.exports = postTagSchema
